35a1dcc3b38363cea51d66e51add534dc387b969
daggerron <cmalfroy@student.42.fr>
2021-07-19 16:33:09 +0200

    Add inject.js + logo

---
+++ assets/js/inject.js
@@ -0,0 +1,2 @@
+window.dataLayer = Object.entries({ js: new Date(), config: 'G-F624SLM32R' })
+document.body.innerHTML += '<script async src="https://www.googletagmanager.com/gtag/js?id=G-F624SLM32R"></script>'
