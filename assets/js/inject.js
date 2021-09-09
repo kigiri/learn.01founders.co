bd7caca59bee88f19bc5441b8280ea0cbc78b989
daggerron <cmalfroy@student.42.fr>
2021-09-09 15:36:17 +0200

    update JS

--- assets/js/inject.js
+++ assets/js/inject.js
@@ -1,2 +1,53 @@
 window.dataLayer = Object.entries({ js: new Date(), config: 'G-F624SLM32R' })
 document.body.innerHTML += '<script async src="https://www.googletagmanager.com/gtag/js?id=G-F624SLM32R"></script>'
+
+const leftContent = document.querySelector('.left')
+const textContent = document.querySelector('.text')
+const contactContent = document.querySelector('.contact')
+
+const contentString = `
+        <span id="zone-01">Zone 01 is our collaborative coding education platform.</span>
+        <br><br>
+        <span id="zone-01-process">New here? Here’s what you need to do:</span>
+        <br><br>
+        1)
+        <span id="instruction-1">Create an account, and log into the platform.</span>
+        <br>
+        2) <span id="instruction-2">Complete the online cognitive test.</span>
+        <br>
+        3)
+        <span id="instruction-3">Wait to find out whether you’ve made it to the next stage.</span>
+        <br>
+        <br>
+        <span id="account">That’s it! If you’ve already created an account, sign in to complete
+          your application or check for updates.</span>
+`
+
+
+const contactString = `
+
+      <div id="about-us">Want to know more about 01 Founders, London's new free coding school, with a job guarantee?&thinsp;</div>
+      <div class="contact">
+      <a id="contact-link" target="_blank" href="https://www.01founders.co/">
+        <span class="arrow">→</span>
+        <span id="contact">Visit our main website.</span>
+      </a>
+      </div>
+      <div class="contact">
+      <a id="contact-link" target="_blank" href="https://www.01founders.co/faq">
+        <span class="arrow">→</span>
+        <span id="faq">Read the Frequently Asked Questions</span>
+      </a>
+      </div>
+      <div class="contact">
+      <a id="contact-link" target="_blank" href="mailto:theteam@01founders.co" style="display:block;">
+        <span class="arrow">→</span>
+        <span id="contact">Contact us.</span>
+      </a>
+      </div>
+`
+
+textContent.innerHTML = contentString
+
+contactContent.insertAdjacentHTML('afterend', contactString)
+contactContent.remove()
