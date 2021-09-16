1bb881458e2d9df1367cc45edd45870bd9e66011
daggerron <cmalfroy@student.42.fr>
2021-09-16 17:22:10 +0200

    "update"

--- assets/js/inject.js
+++ assets/js/inject.js
@@ -1,5 +1,21 @@
 document.body.innerHTML += '<script async src="https://www.googletagmanager.com/gtag/js?id=G-N86JQSKK9B"></script>'
-
+document.body.innerHTML += `
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2942291289344991');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=2942291289344991&ev=PageView&noscript=1"
+/></noscript>
+`
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
